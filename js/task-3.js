92408641a3e347fdff7c22557dc683dabfe83120
vityayakymchuk <147533752+vityayakymchuk@users.noreply.github.com>
2024-01-18 17:25:58 +0100

    push

--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,22 @@
 "use strict";
 
-function getElementWidth(content, padding, border) {
-    const contentNumber = Number.parseFloat(content);
-    const paddingNumber = Number.parseFloat(padding);
-    const borderNumber = Number.parseFloat(border);
-    return contentNumber + paddingNumber * 2 + borderNumber * 2;
+function checkForSpam(message) {
+    const messageLower = message.toLowerCase()
+    if (messageLower.includes("spam")) {
+        return true
+    }
+    else if (messageLower.includes("sale")) {
+        return true
+    }
+    else {
+        return false
+    }
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
